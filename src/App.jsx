8a4b58df7dc96fe1e8bce91f9faaadd589070d80
mile <darimile@gmail.com>
2024-08-07 22:19:02 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./components/Header.jsx"
+
+function App() { // Script setup en Vue
+
+    
+
+    return ( // Template en Vue
+      <>
+        <div className="m-20 max-w-lg mx-auto bg-white shadow p-10">
+
+          
+          <Header /> 
+
+          
+        </div>
+      </>
+
+    
+  )
+}
+
+export default App
