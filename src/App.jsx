b9fffb35476339fee1d44cde9c41b0a70cd57fd4
mile <darimile@gmail.com>
2024-08-09 11:08:34 +0200

    add total

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./components/Header.jsx"
 import Button from "./components/Button.jsx"
-import {formatearDinero} from './helpers'
+import { formatearDinero, calcularTotalPagar } from './helpers'
 
 function App() { // Script setup en Vue
 
   const [cantidad, setCantidad] = useState(10000);
   const [meses, setMeses] = useState(6);
+  const [total, setTotal] = useState(0);
+  const [pago, setPago] = useState(0);
+
+  useEffect(()=> {
+    const resultadoTotalPagar = calcularTotalPagar(cantidad, meses);
+    setTotal(resultadoTotalPagar);
+
+  },[cantidad, meses]);
+
+  useEffect(()=> {
+    // Calcular el pago mensual
+    setPago(total/ meses);
+
+  },[total]);
 
   const MIN = 0;
   const MAX = 20000;
@@ -20,7 +34,7 @@ function App() { // Script setup en Vue
   function handleClickDecremento() {
     const valor = cantidad - STEP;
 
-    if(valor < MIN){
+    if (valor < MIN) {
       alert('Cantidad no válida');
       return
     }
@@ -30,12 +44,11 @@ function App() { // Script setup en Vue
   function handleClickIncremento() {
     const valor = cantidad + STEP;
 
-    if(valor > MAX){
+    if (valor > MAX) {
       alert('Cantidad no válida');
       return
     }
     setCantidad(valor);
-
 
   }
 
@@ -43,57 +56,68 @@ function App() { // Script setup en Vue
     <>
       <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
 
-
         <Header />
 
-
         <div className='flex justify-between my-6'>
-          <Button 
+          <Button
             operador='-'
             fn={handleClickDecremento}
-          
+
           />
-          
-          <Button 
+
+          <Button
             operador='+'
             fn={handleClickIncremento}
-          
+
           />
         </div>
 
-      <input
-        type='range'
-        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
-        onChange={handleChange}
-        min={MIN}
-        max={MAX}
-        step={STEP}
-        value={cantidad}
+        <input
+          type='range'
+          className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+          onChange={handleChange}
+          min={MIN}
+          max={MAX}
+          step={STEP}
+          value={cantidad}
 
-      />
+        />
 
-      <p className="text-center my-10 text-5xl font font-extrabold text-indigo-600">
-        {formatearDinero(cantidad)}  
-      </p>
+        <p className="text-center my-10 text-5xl font font-extrabold text-indigo-600">
+          {formatearDinero(cantidad)}
+        </p>
 
-      <h2 className="text-2xl font-extrabold text-gray-500 text-center" >
-        Elige un <span className="text-indigo-600">Plazo</span> a pagar
-      </h2>
+        <h2 className="text-2xl font-extrabold text-gray-500 text-center" >
+          Elige un <span className="text-indigo-600">Plazo</span> a pagar
+        </h2>
 
-      <select
-            className="w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-lg font-bold text-gray-500 mt-5"
-            value={meses}
-            onChange={ e => setMeses(+e.target.value)}
+        <select
+          className="w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-lg font-bold text-gray-500 mt-5"
+          value={meses}
+          onChange={e => setMeses(+e.target.value)}
         >
-            <option value="6">6 Meses</option>
-            <option value="12">12 Meses</option>
-            <option value="24">24 Meses</option>
+          <option value="6">6 Meses</option>
+          <option value="12">12 Meses</option>
+          <option value="24">24 Meses</option>
         </select>
 
-    </div >
-      </>
 
-    
+        <div className="my-5 space-y-3 bg-gray-50 p-5">
+          <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+            Resumen <span className="text-indigo-600">de pagos</span>
+          </h2>
+
+          <p className="text-xl text-500 text-center font-bold">{meses} Meses</p>
+          <p className="text-xl text-500 text-center font-bold">Total a pagar: {formatearDinero(total)}</p>
+          <p className="text-xl text-500 text-center font-bold">Mensuales: {formatearDinero(pago)}</p>
+        </div>
+
+      </div >
+
+
+    </>
+
+
   )
 }
 
